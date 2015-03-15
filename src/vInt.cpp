aab430ef9b4b729399bdc79d7875334bbb1b6f01
Lenin <ttvlenin@gmail.com>
2015-03-15 16:45:56 -0600

    Carpeta src con vObject y vInt

---
+++ src/vInt.cpp
@@ -0,0 +1,15 @@
+/*
+ * vInt.cpp
+ *
+ *  Created on: 15/3/2015
+ *      Author: tvlenin
+ */
+
+#include "vInt.h"
+
+vInt::vInt(void* pData)  {
+
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ src/vInt.h
@@ -0,0 +1,18 @@
+/*
+ * vInt.h
+ *
+ *  Created on: 15/3/2015
+ *      Author: tvlenin
+ */
+
+#ifndef VINT_H_
+#define VINT_H_
+
+#include "vObject.h"
+
+class vInt: public vObject {
+public:
+	vInt(void * pData);
+};
+
+#endif /* VINT_H_ */
