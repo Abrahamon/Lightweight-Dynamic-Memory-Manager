3c105d06cc58a6744024d7fe6ccd6ce41c0fe9db
Abraham Arias <ipseabraham@gmail.com>
2015-03-18 15:37:56 -0600

    Vheap.h

--- src/singletone.h
+++ src/singletone.h
@@ -4,12 +4,34 @@
  *  Created on: Mar 17, 2015
  *      Author: abraham
  */
+
+#ifndef SRC_SINGLETONE_H_
+#define SRC_SINGLETONE_H_
+
 #include "vHeap.h"
 
 class singletone{
 
 private:
-public:
 	static vHeap* instancia;
 
+public:
+
+	static vHeap* getInstancia()
+	{
+		if( instancia != 0)
+		{
+			return instancia;
+		}else{
+			instancia = new vHeap(200,200);
+
+			return instancia;
+		}
+	};
+
 };
+
+vHeap* singletone::instancia = 0;
+
+#endif /* SRC_SINGLETONE_H */
+
